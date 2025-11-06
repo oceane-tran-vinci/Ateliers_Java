855c23f99305e46a99b730053b47833002e10627
oceane.tran <thuyduong.tran@student.vinci.be>
2025-11-06 22:21:53 +0100

    Atelier 7 : 4.3) Classes

--- AJ_atelier7/src/main/java/be/vinci/ipl/api/Classes.java
+++ AJ_atelier7/src/main/java/be/vinci/ipl/api/Classes.java
@@ -16,7 +16,15 @@ public class Classes {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonStructure getClassInfo(@QueryParam("classname") String classname) {
-        ClassAnalyzer analyzer = new ClassAnalyzer(User.class);
-        return analyzer.getFullInfo();
+        //1) modifier ClassAnalyzer(User.class) pr qu'on puisse choisir nous mm la classe
+        //2) si classe existe pas => throw new WebApplicationException(404); => try catch
+        try {
+            //créer variable pour Stocker la classe demandée par l'utilisateur, chargée dynamiquement
+            Class<?> targetClass = Class.forName("be.vinci.ipl.classes." + classname);
+            ClassAnalyzer analyzer = new ClassAnalyzer(targetClass);
+            return analyzer.getFullInfo();
+        } catch (ClassNotFoundException e){ //méthode forName a besoin d'un catch ClassNotFoundException
+            throw new WebApplicationException(404);
+        }
     }
 }
