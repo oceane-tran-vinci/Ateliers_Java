ba05b23c4d21ca6931e232d6af80c19a00501890
oceane.tran <thuyduong.tran@student.vinci.be>
2025-11-29 12:07:35 +0100

    Atelier 9 : 4.3 Lecture au clavier

--- AJ_atelier09/src/main/java/main/Main.java
+++ AJ_atelier09/src/main/java/main/Main.java
@@ -4,34 +4,40 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
   public static void main(String[] args) {
-    // 1. Création du client HTTP (Factory newHttpClient)
+    // Création du client HTTP (Factory newHttpClient)
     HttpClient client = HttpClient.newHttpClient(); // factory
 
-    // 2. Création de la requête GET
-    HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://openjdk.org/"))
-        .GET()
-        .build();
+    // Lecture de l'URL au clavier avec try-with-resources
+    try (Scanner scanner = new Scanner(System.in)) {
 
-    System.out.println("Envoi de la requête à https://openjdk.org/...");
-    // 3. Envoi asynchrone de la requête
-    CompletableFuture<HttpResponse<String>> future =
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+      System.out.print("Entrez une URL : ");
+      String url = scanner.nextLine();
 
-    // 4. Traitement du résultat lorsque la requête est terminée
-    future.thenAccept(response -> {
-      System.out.println("Status code : " + response.statusCode());
-      System.out.println("HTML :");
-      System.out.println(response.body());
-    });
+      // Construction de la requête GET avec l'URL entrée
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create(url))
+          .GET()
+          .build();
 
-    // Important : empêcher la JVM de s'arrêter avant la fin de la requête
-    future.join();
+      System.out.println("Envoi de la requête à " + url + "...");
+
+      CompletableFuture<HttpResponse<String>> future =
+          client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+      //Envoi asynchrone de la requête
+      future.thenAccept(response -> {
+        System.out.println("Status code : " + response.statusCode());
+        System.out.println("HTML :");
+        System.out.println(response.body());
+      });
+
+      future.join(); // attendre la fin
+    }
 
   }
 }
